604832b5a92a1786bdc5b47d3719da327261c561
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2022-03-19 00:59:29 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+print("같이 협업합시다.")
+a = input("이름을 입력하세요: ")
+b = input("학번을 입력하세요: ")
+c = input("학과를 입력하세요 : ")
+print(a,b,c)
+print("이수경 202102478 컴퓨터공학부")
+print("Hello World!") # print() 함수를 이용하여 "Hello World!"라는 문장을 출력한다.
+# Sugyeong Lee, 202102478, Computer Engineering
+
+print("Sugyeong Lee, 202102478, Computer Engineering")
