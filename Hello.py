edb3d60c9c2ebfc2740033b8869009dcc8b97175
Lee-sugyeong <80825179+Lee-sugyeong@users.noreply.github.com>
2022-03-18 21:16:13 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,6 +1,7 @@
 Touched by user2
 
 print("같이 협업합시다.")
+print("이수경 202102478 컴퓨터공학부")
 print("Hello World!") # print() 함수를 이용하여 "Hello World!"라는 문장을 출력한다.
 # Sugyeong Lee, 202102478, Computer Engineering
 
