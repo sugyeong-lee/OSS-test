743f12f8d77e495a186ad1d6eb82532e7050b3a0
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2022-03-19 01:18:50 +0900

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-print("같이 협업합시다.")
-a = input("이름을 입력하세요: ")
-b = input("학번을 입력하세요: ")
-c = input("학과를 입력하세요 : ")
-print(a,b,c)
-print("이수경 202102478 컴퓨터공학부")
-print("Sugyeong Lee, 202102478, Computer Engineering")
