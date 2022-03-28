4fa43374264ec1f321f53caa3bfa60b4443c92ac
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2022-03-29 00:46:28 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,6 +6,7 @@ b = input("학번을 입력하세요: ")
 c = input("학과를 입력하세요 : ")
 print(a,b,c)
 print("이수경 202102478 컴퓨터공학부")
+print("202102478_이수경_컴퓨터공학과")
 input("학교를 입력해주세요:")
 print("Hello World!") # print() 함수를 이용하여 "Hello World!"라는 문장을 출력한다.
 # Sugyeong Lee, 202102478, Computer Engineering
