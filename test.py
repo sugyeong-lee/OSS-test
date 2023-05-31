af90282e2d83a0fb00ec4c0bf1df829c60495a9a
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2023-05-31 15:17:46 +0900

    레포지토리 정리 중

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
-print("같이 협업합시다.")
-a = input("이름을 입력하세요: ")
-b = input("학번을 입력하세요: ")
-c = input("학과를 입력하세요 : ")
-print('------------')
-print("Hello,",a,b,c) 
-print("이수경 202102478 컴퓨터공학부")
-print("Sugyeong Lee, 202102478, Computer Engineering")
-# Sugyeong Lee, 202102478, Computer Engineering
+print("hello world!")
+print("드디어 로컬과 연결 성공!!!!")
 
-오픈소스 2022-1
+# git 교과서
+
+##git
+# 초기화
+git init
+# 들어가기
+cd ,,,
+# 깃허브 연결 확인
+git remote -v
+# 저장소 끊기
+git remote remove origin
+# 저장소 연결
+git remote add origin " "  (shift+insert)
+# 저장소 기존 내용 가져오기
+git clone""
+# 전체 스테이지 올리기
+git add .
+# 커밋+ 메세지
+git commit -m "커밋할 내용"
+# 깃허브에 추가하기
+git push origin main
+# git 삭제하기
+git rm
+# 
